2e9a9203a25a73b746897e5af3f572cc23db4248
quanpersie2001 <quanpersie001@gmail.com>
2021-12-06 23:34:14 +0700

    Add contructor and toString

--- src/model/Tube.java
+++ src/model/Tube.java
@@ -7,11 +7,30 @@ public class Tube {
     private final static Integer TubeSize = 4;
     private Stack<Block> tube = new Stack<>();
 
+    public Tube() {
+    }
+
+    public Tube(Stack<Block> tube){
+        this.tube = tube;
+    }
+
+    public Tube(Block... blocks ){
+        for (Block block : blocks) {
+            addBlock(block);
+        }
+    }
+
     public Boolean push(Block block) {
         if (isFull()) {
             return false;
         }
         tube.push(block);
+        return true;
+    }
+
+    public Boolean addBlock(Block block){
+        if (isFull()) return false;
+        tube.add(block);
         return true;
     }
 
@@ -48,4 +67,8 @@ public class Tube {
         return true;
     }
 
+    @Override
+    public String toString() {
+        return this.tube.toString();
+    }
 }
