f4aac4ad0f7a2a9a497c0ffda9add92b378d1bdf
quanpersie2001 <quanpersie001@gmail.com>
2021-12-12 21:56:02 +0700

    update color of block

--- src/utils/MyColor.java
+++ src/utils/MyColor.java
@@ -3,13 +3,26 @@ package utils;
 import java.awt.*;
 
 public class MyColor {
-    public static final Color RED = Color.RED;
-    public static final Color BLUE = Color.BLUE;
-    public static final Color GREEN = Color.GREEN;
-    public static final Color YELLOW = Color.YELLOW;
-    public static final Color CYAN = Color.CYAN;
-    public static final Color PINK = Color.pink;
+    public static final Color RED = new Color(227, 38, 54);
+    public static final Color BLUE = new Color(76, 111, 252);
+    public static final Color GREEN = new Color(56, 182, 37);
+    public static final Color YELLOW = new Color(255, 250, 85);
+    public static final Color CYAN = new Color(84, 255, 252);
+    public static final Color PINK = new Color(228, 106, 130);
     public static final Color WHITE = Color.WHITE;
-    public static final Color VIOLET = new Color(102,0,153);
-    public static final Color ORANGE = new Color(255,153,0);
+    public static final Color VIOLET = new Color(161,92,197);
+    public static final Color ORANGE = new Color(255,165,0);
+    public static final Color NAVY = new Color(34,44,104);
+    public static final Color AZURE = new Color(86,153,246);
+    public static final Color CORAL = new Color(205,91,69);
+    public static final Color EGGPLANT = new Color(93,50,141);
+    public static final Color FERN = new Color(79,121,66);
+    public static final Color HELIOTROPE = new Color(227,131,255);
+    public static final Color INDIGO = new Color(75,0,130);
+    public static final Color JADE = new Color(0,168,107);
+    public static final Color LIME = new Color(159,225,41);
+    public static final Color TEAL = new Color(67,148,121);
+    public static final Color KHAKI = new Color(240,230,140);
+    public static final Color MAGENTA = new Color(243,59,141);
+    public static final Color TUBE_COLOR = new Color(161, 161, 161);
 }
--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -24,6 +24,30 @@ public class Utils {
                 return new Block(MyColor.WHITE);
             case "v":
                 return new Block(MyColor.VIOLET);
+            case "n":
+                return new Block(MyColor.NAVY);
+            case "m":
+                return new Block(MyColor.MAGENTA);
+            case "a":
+                return new Block(MyColor.AZURE);
+            case "d":
+                return new Block(MyColor.CORAL);
+            case "e":
+                return new Block(MyColor.EGGPLANT);
+            case "f":
+                return new Block(MyColor.FERN);
+            case "h":
+                return new Block(MyColor.HELIOTROPE);
+            case "k":
+                return new Block(MyColor.KHAKI);
+            case "i":
+                return new Block(MyColor.INDIGO);
+            case "j":
+                return new Block(MyColor.JADE);
+            case "t":
+                return new Block(MyColor.TEAL);
+            case "l":
+                return new Block(MyColor.LIME);
             default:
                 return null;
         }
