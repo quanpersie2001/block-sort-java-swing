8a70163256f2d01ca27d1f4714ca767b5913c21c
quanpersie2001 <quanpersie001@gmail.com>
2021-12-17 04:20:27 +0700

    Add font style constant

---
+++ src/utils/FontStyle.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class FontStyle {
+    public static final String BOLD = "Bold";
+    public static final String EXTRA_BOLD = "EXtraBold";
+    public static final String SEMI_BOLD = "SemiBold";
+    public static final String MEDIUM_ITALIC = "MediumItalic";
+}
